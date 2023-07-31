ba3877a9b6615fbd1ad60e450e12e6ea85500bd1
Kamyar Taghavi <caracicatriz.kt@gmail.com>
2023-07-31 17:59:34 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+const multiply= num1 * num2;
+
+const random = Math.floor(Math.random() * 10 + 1);
+
+const num3 = 20
+const num4 = 5
+const mod = num3/num4;
+
+const max = Math.max(1,3,5,20);
